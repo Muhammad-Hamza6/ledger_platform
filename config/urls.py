82a177009dbe0d1995bbfd400ae84076950b647c
Muhammad-Hamza6 <hamzaasifhamza169@gmail.com>
2026-08-26 17:55:28 +0500

    fixed bug in urls where a / was missing

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/auth", include("apps.users.urls")),
+    path("api/v1/auth/", include("apps.users.urls")),
     path("api/v1/", include("apps.ledger.urls", namespace="ledger")),
 ] + debug_toolbar_urls()
